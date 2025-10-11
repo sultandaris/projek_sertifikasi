c855591004d70c1284f6faaf4b78d9ff8dfd33e9
sultandaris <kishouarima94@gmail.com>
2025-10-11 12:47:16 +0700

    Implement Book management features with controller, model, migration, and view

---
+++ mylib/resources/views/books.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Postingan</title>
+    <style>
+        body { font-family: sans-serif; }
+        .post { border-bottom: 1px solid #ccc; padding: 10px 0; }
+    </style>
+</head>
+<body>
+
+    <h1>Semua Postingan</h1>
+
+    @foreach ($books as $book)
+        <div class="post">
+            {{-- Tampilkan judul post --}}
+            <h2>{{ $book->judul }}</h2>
+
+            {{-- Tampilkan isi post --}}
+            <p>{{ $book->penulis }}</p>
+            <p>{{ $book->tahun_terbit }}</p>
+            <p>{{ $book->tersedia }}</p>
+             {{-- Ganti 'body' dengan nama kolom di tabelmu --}}
+        </div>
+    @endforeach
+
+</body>
+</html>
